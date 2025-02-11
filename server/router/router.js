5bec6b3c66b0c65db286cd3081b35b9d1bf49b6d
Vikram-Rai0 <ccodework24@gmail.com>
2025-02-11 09:18:54 +0545

    added review

--- server/router/router.js
+++ server/router/router.js
@@ -4,6 +4,7 @@ import { event } from "../controller/events.js";
 import {tickets } from "../controller/ticket.js";
 import { checkins } from "../controller/checkins.js";
 import { payments } from "../controller/payment.js";
+import { reviews } from "../controller/review.js";
 
 const routes = express.Router();
 
@@ -12,5 +13,6 @@ routes.post("/event",event);
 routes.post("/ticket",tickets);
 routes.post("/checkins",checkins);
 routes.post("/payment",payments);
+routes.post("/review",reviews);
 
 export default routes;
